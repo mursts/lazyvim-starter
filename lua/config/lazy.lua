aeec6058ac09d8d1fba2ad0d40ebde6eb939842d
mursts <mursts@gmail.com>
2025-05-15 02:26:57 +0900

    add settings

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,10 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
+    { import = "lazyvim.plugins.extras.coding.nvim-cmp" },
+
+    { import = "lazyvim.plugins.extras.editor.refactoring" },
+
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
