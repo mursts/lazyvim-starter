2f5a17be5facb4d0b96fdfa545c4a0ea69606ab6
mursts <mursts@gmail.com>
2025-09-30 01:06:25 +0900

    add clipboard setting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,3 +15,20 @@ opt.sidescrolloff = 5
 
 opt.splitbelow = true
 opt.splitright = true
+
+opt.clipboard = "unnamedplus"
+
+if vim.fn.has("wsl") then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --crlf",
+      ["*"] = "win32yank.exe -o --crlf",
+    },
+    cache_enable = 0,
+  }
+end
